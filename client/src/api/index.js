7b057ebab558929b318124bc2df7fe37e79ff091
Akash <150268192+akash2904r@users.noreply.github.com>
2024-05-02 18:16:17 +0530

    Delete client directory

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:3000/posts'
-
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, updatePost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
--- client/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const inputList = [
-    { name: 'Creator', isFocused: false },
-    { name: 'Title', isFocused: false },
-    { name: 'Message', isFocused: false },
-    { name: 'Tags', isFocused: false },
-];
